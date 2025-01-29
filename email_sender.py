55ff8425e959f901c7ea2d32153ed8aac879903f
Roland <rollycrompton@gmail.com>
2025-01-29 10:36:51 +0000

    implemented emailing

--- email_sender.py
+++ email_sender.py
@@ -9,7 +9,13 @@ queue = os.getenv("LOW_PRIORITY_QUEUE")
 access_id = os.getenv("AWS_ACCESS_KEY_ID")
 access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 aws_region = os.getenv("AWS_REGION")
+email = os.getenv("EMAIL")
+
 sqs = boto3.client("sqs",
+                   region_name=aws_region,
+                   aws_access_key_id=access_id,
+                   aws_secret_access_key=access_key)
+ses = boto3.client("ses",
                    region_name=aws_region,
                    aws_access_key_id=access_id,
                    aws_secret_access_key=access_key)
@@ -36,5 +42,15 @@ def get_message():
     )
     send_email(message)
 
-def send_email:
-    return
+def send_email(message):
+    print(message)
+    message_json = json.loads(message["Body"])
+    ses.send_email(Source=email,
+                   Destination={"ToAddresses": [email]},
+                   Message={"Subject":
+                                {"Data": f"{message_json['priority']} priority - {message_json['title']}"},
+                            "Body":
+                                {"Text": {"Data": message_json['message']}}})
+
+while True:
+    get_message();
