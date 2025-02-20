3c6e63a8e7b29c40d4ef3b885d361f0899e4d15f
Roland <rollycrompton@gmail.com>
2025-02-20 15:49:34 +0000

    Brought in line with other queues

--- test/test_email_sender.py
+++ test/test_email_sender.py
@@ -4,18 +4,17 @@ import time
 import boto3
 import pytest
 
-from emails import email_sender
+import emails
+email_consumer = emails.email_sender
+consumer = email_consumer.consumer
 
 from moto import mock_aws
 
 
-default_email_method  = email_sender.send_email
+default_email_method  = email_consumer.send
 
 message_body = '{"priority": "high", "title": "message title", "message": "this is a message body"}'
 received_message = None
-
-def replacement_send(message):
-    pass
 
 @mock_aws
 def test_get_message():
@@ -24,7 +23,7 @@ def test_get_message():
                          DelaySeconds=0,
                          MessageBody=message_body)
 
-    retrieved_message = email_sender.get_from_queue()
+    retrieved_message = consumer.get_from_queue()
 
     assert retrieved_message is not None
 
@@ -37,68 +36,71 @@ def test_delete_message():
                          DelaySeconds=0,
                          MessageBody=message_body)
 
-    retrieved_message = email_sender.get_from_queue()
-    email_sender.delete(retrieved_message)
+    retrieved_message = consumer.get_from_queue()
+    consumer.delete(retrieved_message)
 
     assert "Message" not in mock_sqs.receive_message(
         QueueUrl=queue,
         MaxNumberOfMessages=1,
         MessageAttributeNames=["All"],
         VisibilityTimeout=0,
-        WaitTimeSeconds=20
+        WaitTimeSeconds=0
     )
 
 @mock_aws
 def test_no_message():
-    sqs = prepare_aws()
+    prepare_aws()
 
-    retrieved_message = email_sender.get_from_queue()
+    retrieved_message = consumer.get_from_queue()
 
     assert retrieved_message is None
 
 
 @mock_aws
-def test_run_without_email():
+def test_process_without_teams():
     sqs = prepare_aws()
     mock_sqs = sqs[0]
     queue = sqs[1]
-    email_sender.send_email = send_email_stub
+    consumer.send = send_to_teams_stub
     mock_sqs.send_message(QueueUrl=queue,
                         DelaySeconds=0,
                         MessageBody=message_body)
-    timer_thread = threading.Thread(target=timer, args=[10]) # Ensure test doesn't run forever if it fails
+    timer_thread = threading.Thread(target=timer, args=[20]) # Ensure test doesn't run forever if it fails
     timer_thread.start()
-    email_sender.run()
-
-
+    consumer.running = True
+    email_consumer.consumer.process()
+    consumer.running = False
+    global received_message
     assert (received_message is not None # Message was received
             and "Message" not in mock_sqs.receive_message( # Message was deleted
         QueueUrl=queue,
         MaxNumberOfMessages=1,
         MessageAttributeNames=["All"],
         VisibilityTimeout=0,
-        WaitTimeSeconds=20
+        WaitTimeSeconds=0
     ))
 
 @mock_aws
 def prepare_aws():
     mock_sqs = boto3.client("sqs", region_name='us-east-1')
     queue = mock_sqs.create_queue(QueueName="team")['QueueUrl']
-    email_sender.sqs = mock_sqs
-    email_sender.queue = queue
+    consumer.sqs = mock_sqs
+    consumer.queue = queue
     return mock_sqs, queue
 
 def timer(seconds):
     time.sleep(seconds)
-    email_sender.running = False
+    consumer.running = False
 
-def send_email_stub(message):
+def send_to_teams_stub(message):
     global received_message
     received_message = message["Body"]
-    email_sender.running = False
+    consumer.running = False
 
 @pytest.fixture(autouse=True)
 def before_each():
-    email_sender.send_email = default_email_method
+    consumer.running = False
+    email_consumer.bg_thread.join()
+    consumer.send = default_email_method
     global received_message
     received_message = None
